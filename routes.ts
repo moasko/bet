249f4c722e037d04c89ec6d82244e5b59710561c
moasko.dev <42101671+moasko@users.noreply.github.com>
2024-11-19 17:06:23 +0000

    Create routes.ts

---
+++ routes.ts
@@ -0,0 +1,14 @@
+export const publicRoutes = [
+  "/",
+  // ajoutez d'autres routes publiques ici
+];
+
+export const authRoutes = [
+  "/auth/signin",
+  "/auth/signup",
+  // ajoutez d'autres routes d'authentification ici
+];
+
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = "/admin";
