cbfa7bb5d03077e83da3e968d5b52efb190fcecf
moasko.dev <42101671+moasko@users.noreply.github.com>
2024-11-19 17:08:22 +0000

    Create signin-action.ts

---
+++ modules/auth/lib/signin-action.ts
@@ -0,0 +1,57 @@
+"use server";
+
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
+export async function signInAction(
+  data: LoginSchemaType
+): Promise<MessageResponse> {
+  const validate = await LoginSchema.safeParseAsync(data);
+  if (!validate.success) {
+    return {
+      message: validate.error.errors[0].message,
+      success: false,
+    };
+  }
+
+  const { email, password } = validate.data;
+  const user = await db.user.findUnique({
+    where: { email },
+  });
+  const ERROR_INVALID_CREDENTIALS = "Invalid credentials";
+  if (!user || !user.email || !user.password) {
+    return {
+      message: ERROR_INVALID_CREDENTIALS,
+      success: false,
+    };
+  }
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+    });
+
+    return {
+      message: "Sign In Sucessfully",
+      success: true,
+    };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { message: ERROR_INVALID_CREDENTIALS, success: false };
+        default:
+          return { message: "Something went wrong!", success: false };
+      }
+    }
+    throw error;
+  }
+}
+
+import { signOut } from "@/auth";
+import db from "@/lib/db";
+import { LoginSchema, LoginSchemaType } from "../auth.schema";
+import { MessageResponse } from "../types/auth";
+
+export { signOut };
